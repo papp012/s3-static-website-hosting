4eac4cc91cfc9a0011967d2d7769fdaab631ca13
papp <papp.csenge0@gmail.com>
2022-10-19 11:27:40 +0200

    feat: upload static files to s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,48 @@
 resource "aws_s3_bucket" "www_bucket" {
-  prefix = "www.${var.prefix}-websitetest.com"
-   
+  bucket = "www.${var.prefix}-websitetest.com"
 }
-
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
   acl = "public-read"
   bucket = aws_s3_bucket.www_bucket.id 
+}
+
+#resource "aws_s3_bucket_object" "object" {
+#  for_each = fileset("../app", "*")
+#
+ # bucket = "www.papp012-websitetest.com"
+  #key    = each.value
+  #source = "../app/${each.value}"
+#}
+
+resource "aws_s3_bucket_object" "index_page" {
+  bucket = "${aws_s3_bucket.www_bucket.id}"
+  key = "index.html"
+  source = "../app/index.html"
+  content_type = "text/html"
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_object" "error_page" {
+  bucket = "${aws_s3_bucket.www_bucket.id}"
+  key = "404.html"
+  source = "../app/404.html"
+  content_type = "text/html"
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_object" "css" {
+  bucket = "${aws_s3_bucket.www_bucket.id}"
+  for_each = fileset("../app/css", "*")
+  key = each.value
+  source = "../app/css/${each.value}"
+  content_type = "text/css"
+}
+
+resource "aws_s3_bucket_object" "pictures" {
+  bucket = "${aws_s3_bucket.www_bucket.id}"
+  for_each = fileset("../app/pictures", "*")
+  key = each.value
+  source = "../app/pictures/${each.value}"
+  content_type = "image/jpeg"
 }
